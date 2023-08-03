c6ff6e0ee8c3b7328fd07373ab5b666ba93a2321
L7cass <lucasgoncalves.assis@gmail.com>
2023-08-03 16:53:36 -0300

    atividade 2  e exemplo de entrada de dados

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,24 @@
+package principal;
+
+//classe principal
+public class OlaMundo {
+
+	//metodo principal, onde executa o programa
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova linha 
+		//print = imprimir; In = new line
+		System.out.println("hello world, Olá Mundo!");
+		
+		//comentario de uma linha 
+		/*
+		 * esse é 
+		 * um comentario 
+		 * de multipla limhas
+		 */
+	
+	
+
+	}
+
+}
