3d81f580d6d657039d8f0a88268a5bf24c971f59
L7cass <lucasgoncalves.assis@gmail.com>
2023-08-04 15:18:43 -0300

    operadores aritimericos

---
+++ OperadoresAritimericos/src/Principal/OperadoresAritimericos.java
@@ -0,0 +1,54 @@
+package Principal;
+
+public class OperadoresAritimericos {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Operador "+" para soma Operador "-" para subtração Operador "*" para
+		 * multiplicação Operador "/" para divisão Operador "%" para o resto da divisão
+		 */
+
+		double numero1 = 110.5;
+		double numero2 = 10.2;
+		double soma = numero1 + numero2;
+		double divisao = numero1 / numero2;
+		double subtração = numero1 - numero2;
+		double multiplicacao = numero1 * numero2;
+		double restoDaDivisao = numero1 % numero2;
+		double calculo = 10 + (10 * 10) / 10;
+
+		System.out.println("Resultado da soma: " + soma);
+		System.out.println("Somando 2 numeros: " + (numero1 + numero2));
+		System.out.println("Divisao de 2 numeros: " + divisao);
+		System.out.println("Subtração de 2 numeros: " + subtração);
+		System.out.println("Multiplicação de 2 numeros: " + multiplicacao);
+		System.out.println("Resto da divisão: " + restoDaDivisao);
+		System.out.println("Calculo: " + calculo);
+
+		/*
+		 * operador "+" para soma operador "-" para subtração operador "*" para
+		 * multiplicação operador "/" para divisão operador "%" para o resto da divisão
+		 */
+
+		int numero3 = 11;
+		int numero4 = 10;
+		int soma2 = numero3 + numero4;
+		int divisao2 = numero3 / numero4;
+		int subtração2 = numero3 - numero4;
+		int multiplicacao2 = numero3 * numero4;
+		int restoDaDivisao2 = numero3 % numero4;
+		int calculo2 = 10 + (10 * 10) / 10;
+
+		System.out.println("                                        ");
+		System.out.println("Resultado da soma: " + soma2);
+		System.out.println("Somando 2 numeros: " + (numero3 + numero4));
+		System.out.println("Divisao de 2 numeros: " + divisao2);
+		System.out.println("Subtração de 2 numeros: " + subtração2);
+		System.out.println("Multiplicação de 2 numeros: " + multiplicacao2);
+		System.out.println("Resto da divisão: " + restoDaDivisao2);
+		System.out.println("Calculo: " + calculo2);
+
+	}
+
+}
